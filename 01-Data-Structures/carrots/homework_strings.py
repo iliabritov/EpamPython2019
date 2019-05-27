7af8907700845697a7998a8188f06bf7f94e2f76
iliabritov <ilia.brittov@gmail.com>
2019-05-27 11:32:20 +0300

    Homework strings

--- 01-Data-Structures/carrots/homework_strings.py
+++ 01-Data-Structures/carrots/homework_strings.py
@@ -1,5 +1,4 @@
 """"
-
 Задание 1
 
 0) Повторение понятий из биологии (ДНК, РНК, нуклеотид, протеин, кодон)
@@ -11,48 +10,89 @@
 
 3) Перевод последовательности РНК в протеин*
 
-
-*В папке files вы найдете файл rna_codon_table.txt - 
-в нем содержится таблица переводов кодонов РНК в аминокислоту, 
-составляющую часть полипептидной цепи белка.
-
-
-Вход: файл dna.fasta с n-количеством генов
-
 Выход - 3 файла:
  - статистика по количеству нуклеотидов в ДНК
  - последовательность РНК для каждого гена
  - последовательность кодонов для каждого гена
 
- ** Если вы умеете в matplotlib/seaborn или еще что, 
- welcome за дополнительными баллами за
- гистограммы по нуклеотидной статистике.
- (Не забудьте подписать оси)
-
-P.S. За незакрытый файловый дескриптор - караем штрафным дезе.
-
 """
-
-# read the file dna.fasta
-dna = None
-
-
-def translate_from_dna_to_rna(dna):
-    
-    """your code here"""
-    
+def translate_from_dna_to_rna(dna):  
+    rna = dna.replace('T', 'U')  
     return rna
 
-
-def count_nucleotides(dna):
-    
-    """your code here"""
-    
+def count_nucleotides(*dna):
+    num_of_nucleotides = {}
+    for line in dna:
+        for element in list(line.replace('\n','')):
+            if element in num_of_nucleotides:
+                num_of_nucleotides[element] += 1
+            else:
+                num_of_nucleotides[element] = 1     
     return num_of_nucleotides
 
 
-def translate_rna_to_protein(rna):
+def translate_rna_to_protein(rna, codons_table):
+    rna = rna.replace('\n', '')
+    protein = ''
+    if len(rna) % 3 == 0:
+        for num in range(0, len(rna), 3):
+            codon = rna[num: num + 3]
+            protein += codons_table[codon]
+    return protein + '\n'
+
+# read the file dna.fasta
+dna = open('.\\files\\dna.fasta', 'r')
+dna_data = dna.readlines()
+dna.close()
+
+# data processing
+dna_sequences = {}
+for data_line in dna_data:
+    if data_line.startswith('>'):
+        dna_sequences[data_line] = []
+        name = data_line
+    else:
+        dna_sequences[name].append(data_line)
+
+# count nucleotites
+dna_statistic = open('.\\files\\result_dna_statistic.txt', 'w')
+for name in dna_sequences:
+    dna_statistic.write(name)
+    curr_count = count_nucleotides(*dna_sequences[name])
+    for elem in curr_count:
+        dna_statistic.write(elem + ' = ' + str(curr_count[elem]) + '\n')
+dna_statistic.close()
     
-    """your code here"""
-    
-    return protein
+# codon's transfer table
+flag = 1
+last_elem = ''
+codons_data = {}
+codons_obj = open('.\\files\\rna_codon_table.txt', 'r')
+codons_lines = codons_obj.readlines()
+for line in codons_lines:
+    for element in line.split():
+        if flag:
+            codons_data[element] = None
+            last_elem = element
+            flag = 0
+        else:
+            codons_data[last_elem] = element
+            flag = 1
+
+# dna to rna + rna to protein 
+dna_to_rna = open('.\\files\\result_dna_to_rna.txt', 'w')
+rna_to_protein = open('.\\files\\result_rna_to_protein.txt', 'w')
+
+for name in dna_sequences:
+    dna_to_rna.write(name)
+    rna_to_protein.write(name)
+    for dna_line in dna_sequences[name]:
+        # dna to rna
+        rna_line = translate_from_dna_to_rna(dna_line)
+        dna_to_rna.write(rna_line)
+        # rna to protein
+        protein_line = translate_rna_to_protein(rna_line, codons_data)
+        rna_to_protein.write(protein_line)
+                   
+dna_to_rna.close()
+rna_to_protein.close()
