6c4d5f9c1da38ceca09a5668d328f0b344966ac9
iliabritov <47694490+iliabritov@users.noreply.github.com>
2019-06-05 01:44:09 +0300

    Update hw1.py

--- 02-Functions/hw/hw1.py
+++ 02-Functions/hw/hw1.py
@@ -1,10 +1,10 @@
 import string
 
+
 def letters_range(start, stop=0, step=1, **change):
     alph = list(string.ascii_lowercase)
     for elem in change:
         alph[alph.index(elem)] = change[elem]
-    
     if stop:
         return alph[alph.index(start):alph.index(stop):step]
     else:
