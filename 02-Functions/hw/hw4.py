fe0e577575a660d16958ab835d9e7fc676b85676
iliabritov <ilia.brittov@gmail.com>
2019-06-05 01:40:46 +0300

    hw4 has beed added

--- 02-Functions/hw/hw4.py
+++ 02-Functions/hw/hw4.py
@@ -1,38 +1,29 @@
 import inspect
 
-def modified_func(func, *fixated_args, **fixated_kwargs):
 
-    #def new_func(*args, **kwargs):
-        
+def modified_func(func, *fixated_args, **fixated_kwargs):
+    def new_func(*fixed_args, **fixed_kwargs):
+        frame = inspect.currentframe()
+        args = inspect.getargvalues(frame)[3]
+        code = inspect.getsource(func)
+        func_name = code[4:code.index('(')]
+        docs = [('A func implementation of ' + func_name + '\n'),
+                'with pre-applied arguments being:\n',
+                ('fixated_args: ' + str(args['fixated_args']) + '\n'),
+                ('fixated_kwargs: ' + str(args['fixated_kwargs']) + '\n'),
+                ('code: \n' + code)]
+        new_func.__doc__ = ''.join(docs)
+        new_func.__name__ = 'func_' + func_name
+        sum_args = [*fixated_args, *fixed_args]
+        sum_kwargs = {**fixated_kwargs, **fixed_kwargs}
+        return func(*sum_args, **sum_kwargs)
     return new_func
 
 
-def func_var(func):
-    """ get all named variable from function """
-    def_fc = inspect.getsourcelines(func)[0][0]
-    def_fc = def_fc[def_fc.index('(') + 1 : def_fc.index(')')]
-    def_fc = def_fc.replace(',', '').split()
-    named_var = []
-    for elem in def_fc:
-        if '=' in elem:
-            named_var.append(elem.split('=')[0])
-    return named_var
 
 
 
-def fc1(a, b):
-    return a + b
-def fc2(a, b, c=4):
-    return a*b*c
-def fc3():
-    print('Useless fc!')
-def fc4(a=1, b=2, c=3):
-    return a ** b ** c
 
 
 
-print(func_var(fc1))
-print(func_var(fc2))
-print(func_var(fc3))
-print(func_var(fc4))
-    
+
