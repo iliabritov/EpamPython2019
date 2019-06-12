d0192d28f414ede712e49c527ff1ebc397435a45
iliabritov <ilia.brittov@gmail.com>
2019-06-12 16:25:36 +0300

    oop_2 hw

--- 05-OOP_Exceptions/hw/oop_2.py
+++ 05-OOP_Exceptions/hw/oop_2.py
@@ -1,56 +1,69 @@
-"""
-В этом задании будем улучшать нашу систему классов из задания прошлой лекции
-(Student, Teacher, Homework)
-Советую обратить внимание на defaultdict из модуля collection для
-использования как общую переменную
-
-
-1. Как то не правильно, что после do_homework мы возвращаем все тот же
-объект - будем возвращать какой-то результат работы (HomeworkResult)
-
-HomeworkResult принимает объект автора задания, принимает исходное задание
-и его решение в виде строки
-Атрибуты:
-    homework - для объекта Homework, если передан не этот класс -  выкинуть
-    подходящие по смыслу исключение с сообщением:
-    'You gave a not Homework object'
-
-    solution - хранит решение ДЗ как строку
-    author - хранит объект Student
-    created - c точной датой и временем создания
-
-2. Если задание уже просрочено хотелось бы видеть исключение при do_homework,
-а не просто принт 'You are late'.
-Поднимайте исключение DeadlineError с сообщением 'You are late' вместо print.
-
-3. Student и Teacher имеют одинаковые по смыслу атрибуты
-(last_name, first_name) - избавиться от дублирования с помощью наследования
-
-4.
-Teacher
-Атрибут:
-    homework_done - структура с интерфейсом как в словаря, сюда поподают все
-    HomeworkResult после успешного прохождения check_homework
-    (нужно гаранитровать остутствие повторяющихся результатов по каждому
-    заданию), группировать по экземплярам Homework.
-    Общий для всех учителей. Вариант ипользования смотри в блоке if __main__...
-Методы:
-    check_homework - принимает экземпляр HomeworkResult и возвращает True если
-    ответ студента больше 5 символов, так же при успешной проверке добавить в
-    homework_done.
-    Если меньше 5 символов - никуда не добавлять и вернуть False.
-
-    reset_results - если передать экземпряр Homework - удаляет только
-    результаты этого задания из homework_done, если ничего не передавать,
-    то полностью обнулит homework_done.
-
-PEP8 соблюдать строго, проверку делаю автотестами и просмотром кода.
-Всем перечисленным выше атрибутам и методам классов сохранить названия.
-К названием остальных переменных, классов и тд. подходить ответственно -
-давать логичные подходящие имена.
-"""
-import datetime
+from datetime import datetime, timedelta
 from collections import defaultdict
+
+
+class Homework():
+
+    def __init__(self, text, number_of_days):
+        self.text = text
+        self.deadline = timedelta(days=number_of_days)
+        self.created = datetime.now()
+
+    def is_active(self):
+        return self.created + self.deadline > datetime.now()
+
+
+class Person():
+
+    def __init__(self, last_name, first_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+class Student(Person):
+
+    def do_homework(self, homework, solution):
+        if homework.is_active():
+            return HomeworkResult(self, homework, solution)
+        raise DeadlineError('You are late')
+
+
+class Teacher(Person):
+    homework_done = defaultdict(dict)
+
+    @staticmethod
+    def create_homework(text, days):
+        return Homework(text, days)
+
+    @classmethod
+    def check_homework(cls, hw_result):
+        solution = hw_result.solution
+        if len(solution) > 5 and hw_result.solution not in cls.homework_done:
+            cls.homework_done[hw_result.homework][hw_result.solution] = hw_result
+            return True
+        return False
+
+    @classmethod
+    def reset_results(cls, hw=None):
+        if hw:
+            del cls.homework_done[hw]
+        else:
+            cls.homework_done = {}
+
+
+class HomeworkResult():
+
+    def __init__(self, autor, homework, solution):
+        self.autor = autor
+        self.homework = homework
+        if not isinstance(homework, Homework):
+            raise TypeError('You gave a not Homework object')
+        self.solution = solution
+        self.created = datetime.now()
+
+
+class DeadlineError(Exception):
+    """This homework not active"""
 
 
 if __name__ == '__main__':
@@ -79,6 +92,5 @@ if __name__ == '__main__':
 
     opp_teacher.check_homework(result_2)
     opp_teacher.check_homework(result_3)
-
     print(Teacher.homework_done[oop_hw])
     Teacher.reset_results()
