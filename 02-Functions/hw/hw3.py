03c1ef597153b33d895462c63bffe5c70f94e659
iliabritov <ilia.brittov@gmail.com>
2019-06-14 08:57:22 +0300

    add decorator

--- 02-Functions/hw/hw3.py
+++ 02-Functions/hw/hw3.py
@@ -3,3 +3,19 @@ def make_it_count(func, counter_name):
         globals()[counter_name] += 1
         return func(*args, **kwargs)
     return new_func
+
+
+def make_it_count1(counter_name):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            globals()[counter_name] += 1
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+@make_it_count1('new1')
+def jd(args):
+    return ' '.join(args)
+
+
